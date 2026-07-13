bd9ee02c17206bf1b55515476899d865296e31b0
DevilloTech <akbgolfo@gmail.com>
2026-07-13 12:31:25 +0800

    Login Button in User Login to Home Fragment / Main Activity for Testing

--- app/src/main/java/com/example/technicalsumm2/UserLoginActivity.kt
+++ app/src/main/java/com/example/technicalsumm2/UserLoginActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.content.Intent
+import android.widget.Button
 
 class UserLoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +21,15 @@ class UserLoginActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
+        val btLogin = findViewById<Button>(R.id.btLogin2)
+        // The code below is just for testing the LOGIN BUTTON to go to the Home Fragment / Main Activity
 
+        btLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        // ----------------------------------------------------------------------------------------------
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
